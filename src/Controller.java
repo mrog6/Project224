892aafc7895efbf26a5078c7253c477727625358
Nick Chua <nickchua@Nicks-MacBook-Pro-5.local>
2019-11-16 20:26:00 -0800

    redo commit

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,89 @@
-public class Controller {
+import javax.swing.*;
+import java.awt.*;
+
+public class Controller extends JPanel {
     private Model model;
     private View view;
+    protected int turn;
+    protected String playerOne;
+    protected String playerTwo;
+    protected Color newColor1;
+    protected Color newColor2;
 
     public Controller(Model model) {
         this.model = model;
         this.view = new View(this);
+        Color c = Color.WHITE;
+        startOver();
 
+        newColor1 = JColorChooser.showDialog(null,
+                playerOne + ", choose your symbol color!",
+                view.banner.getBackground());
+        if (newColor1 == null || newColor1.equals(c)) {
+            view.playerOneName.setForeground(Color.BLACK);
+        }
+        else {
+            view.playerOneName.setForeground(newColor1);
+        }
 
+        newColor2 = JColorChooser.showDialog(null,
+                playerTwo + ", choose your symbol color!",
+                view.banner.getBackground());
+        if (newColor2 == null || newColor2.equals(c)) {
+            view.playerTwoName.setForeground(Color.BLACK);
+        }
+        else {
+            view.playerTwoName.setForeground(newColor2);
+        }
+        view.buttons[0][0].setBackground(newColor1);
+
+        //view.statusLabel.setForeground(newColor);
+        //^^ do that with the buttons each turn :)
+    }
+
+    public void startOver() {
+        playerOne = JOptionPane.showInputDialog("Please enter player 1's name");
+        if (playerOne.length() != 0)
+            view.playerOneName.setText(playerOne);
+        else
+            view.playerOneName.setText("Player 1");
+
+        playerTwo = JOptionPane.showInputDialog("Please enter player 2's name");
+        if (playerTwo.length() != 0)
+            view.playerTwoName.setText(playerTwo);
+        else
+            view.playerTwoName.setText("Player 2");
+
+        this.turn = 1;
+        view.statusLabel.setText(view.playerOneName.getText() + "'s turn");
+    }
+
+    public void pressButton(int i, int j) {
+        if (!model.validMove(i, j)) {
+            if (turn % 2 == 1) {
+                view.statusLabel.setText("Invalid Move. " + view.playerOneName.getText() + "'s turn");
+            }
+            else if (turn % 2 == 0) {
+                view.statusLabel.setText("Invalid Move. " + view.playerTwoName.getText() + "'s turn");
+            }
+            return;
+        }
+        else {
+            model.insertSymbol(i, j, turn);
+
+            if (turn % 2 == 1) {
+                view.buttons[i][j].setText("X");
+                turn++;
+                view.statusLabel.setText(view.playerTwoName.getText() + "'s turn");
+                view.buttons[i][j].setForeground(newColor1);
+            }
+            else if (turn % 2 == 0) {
+                view.buttons[i][j].setText("O");
+                turn++;
+                view.statusLabel.setText(view.playerOneName.getText() + "'s turn");
+                view.buttons[i][j].setForeground(newColor2);
+            }
+        }
     }
 
     public static void main(String[] args) {
--- src/View.java
+++ src/View.java
@@ -1,24 +1,129 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class View extends JFrame {
+public class View extends JFrame implements ActionListener {
     private Controller controller;
+    protected JLabel statusLabel;
+    protected JLabel banner;
+    protected JLabel playerOneName;
+    protected JLabel playerTwoName;
+    protected JLabel playerOneWins;
+    protected JLabel playerOneLosses;
+    protected JLabel playerTwoWins;
+    protected JLabel playerTwoLosses;
+    protected JPanel contentPanel;
+    protected JButton resetButton;
+    protected JButton saveExitButton;
+    protected JButton[][] buttons;
+    protected final String PLAYER_ONE_SYMBOL = "X";
+    protected final String PLAYER_TWO_SYMBOL = "O";
+
 
     public View(Controller controller) {
-        super("");
+        super("Go-Moku");
         this.controller = controller;
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(500, 500));
+        setPreferredSize(new Dimension(800, 1000));
         setupUI();
         pack();
     }
 
     public void setupUI() {
-        JPanel contentPanel = new JPanel();
+        contentPanel = (JPanel) getContentPane();
+        banner = new JLabel("Welcome to Color Chooser!");
 
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
+        // layout for the top half of the app: two gridLayouts for the players inside of a boxLayout
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        topPanel.setPreferredSize(new Dimension(800, 100));
 
-        getContentPane().add(contentPanel);
+        // player 1 info box
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new GridLayout(3, 2));
+        leftPanel.setBorder(BorderFactory.createTitledBorder("Player 1 (X)"));
+        JLabel playerOneNameLabel = new JLabel("Name:");
+        playerOneName = new JLabel(" ");
+        JLabel playerOneWinLabel = new JLabel("Wins:");
+        playerOneWins = new JLabel("0");
+        JLabel playerOneLossLabel = new JLabel("Losses:");
+        playerOneLosses = new JLabel("0");
+        leftPanel.add(playerOneNameLabel);
+        leftPanel.add(playerOneName);
+        leftPanel.add(playerOneWinLabel);
+        leftPanel.add(playerOneWins);
+        leftPanel.add(playerOneLossLabel);
+        leftPanel.add(playerOneLosses);
+
+        // player 2 info box
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(3, 2));
+        rightPanel.setBorder(BorderFactory.createTitledBorder("Player 2 (O)"));
+        JLabel playerTwoNameLabel = new JLabel("Name:");
+        playerTwoName = new JLabel(" ");
+        JLabel playerTwoWinLabel = new JLabel("Wins:");
+        playerTwoWins = new JLabel("0");
+        JLabel playerTwoLossLabel = new JLabel("Losses:");
+        playerTwoLosses = new JLabel("0");
+        rightPanel.add(playerTwoNameLabel);
+        rightPanel.add(playerTwoName);
+        rightPanel.add(playerTwoWinLabel);
+        rightPanel.add(playerTwoWins);
+        rightPanel.add(playerTwoLossLabel);
+        rightPanel.add(playerTwoLosses);
+
+        // encapsulates the player info boxes into the topPanel
+        topPanel.add(leftPanel);
+        topPanel.add(rightPanel);
+
+        statusLabel = new JLabel("Welcome to GO-MOKU!");
+        statusLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        statusLabel.setFont(new Font(" ", Font.BOLD, 20));
+
+        // layout for the bottom half of the app: one gridlayout for the buttons inside of a boxLayout
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        bottomPanel.setPreferredSize(new Dimension(800, 800));
+        // grid for the JButtons
+        JPanel gridPanel = new JPanel();
+        gridPanel.setLayout(new GridLayout(15, 15));
+        buttons = new JButton[15][15];
+        for(int i = 0; i < 15; i++) {
+            for(int j=0; j<15; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setText(" ");
+                buttons[i][j].setFont(new Font(" ", Font.BOLD, 20));
+                buttons[i][j].setPreferredSize(new Dimension(3, 3));
+                buttons[i][j].addActionListener(this);
+                buttons[i][j].setOpaque(true);
+                gridPanel.add(buttons[i][j]);
+            }
+        }
+        bottomPanel.add(gridPanel);
+
+        JPanel lastPanel = new JPanel();
+        resetButton = new JButton("Reset Names and Stats");
+        saveExitButton = new JButton("Save and Exit");
+        lastPanel.add(saveExitButton);
+        lastPanel.add(resetButton);
+
+        // encapsulates both panels into the app
+        contentPanel.add(topPanel);
+        contentPanel.add(statusLabel);
+        contentPanel.add(bottomPanel);
+        contentPanel.add(lastPanel);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        for(int i = 0; i < 15; i++) {
+            for(int j=0; j<15; j++) {
+                if (e.getSource() == buttons[i][j])
+                    controller.pressButton(i, j);
+            }
+        }
     }
 }
