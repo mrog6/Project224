fb57c0f5ff748ea4fede483f4162459fdd1c9013
Nick Chua <nickchua@cjca-23915d.gonzaga.edu>
2019-11-25 09:44:42 -0800

    redo commit vertical win

--- src/Controller.java
+++ src/Controller.java
@@ -1,21 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Controller extends JPanel {
     private Model model;
     private View view;
+    protected Timer timer;
+    static final int DELAY = 5000;
     protected int turn;
     protected String playerOne;
     protected String playerTwo;
     protected Color newColor1;
     protected Color newColor2;
+    Color c = Color.WHITE;
 
     public Controller(Model model) {
         this.model = model;
         this.view = new View(this);
-        Color c = Color.WHITE;
         startOver();
 
+        view.resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.statusLabel.setForeground(Color.BLACK);
+                model.emptyBoard();
+                for (int k = 0; k < 15; k++) {
+                    for (int j = 0; j < 15; j++) {
+                        view.buttons[k][j].setText(" ");
+                        view.buttons[k][j].setForeground(Color.WHITE);
+                        view.buttons[k][j].setBackground(Color.WHITE);
+                    }
+                }
+                startOver();
+            }
+        });
+
+        view.saveExitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        timer = new Timer(DELAY, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("timer");
+            }
+        });
+        timer.start();
+
+    }
+
+    public void chooseColor() {
         newColor1 = JColorChooser.showDialog(null,
                 playerOne + ", choose your symbol color!",
                 view.banner.getBackground());
@@ -35,13 +73,26 @@ public class Controller extends JPanel {
         else {
             view.playerTwoName.setForeground(newColor2);
         }
-        view.buttons[0][0].setBackground(newColor1);
+    }
 
-        //view.statusLabel.setForeground(newColor);
-        //^^ do that with the buttons each turn :)
+    public void openingMessage() {
+        String[] options = {"Friend", "Computer", "Cancel"};
+        int choice = JOptionPane.showOptionDialog(null, "The object of the game is to get 5 in a row. " +
+                "Each player will have five seconds to make a move. Would you like to play against the computer or a friend?",
+                "WELCOME TO GO MOKU!", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                null, options,
+                options[0]);
+//        if (choice == JOptionPane.YES_OPTION) {
+//
+//        }
+//        if (choice == JOptionPane.NO_OPTION)
+//            System.exit(0);
+//        else
+//            System.exit(0);
     }
 
     public void startOver() {
+        openingMessage();
         playerOne = JOptionPane.showInputDialog("Please enter player 1's name");
         if (playerOne.length() != 0)
             view.playerOneName.setText(playerOne);
@@ -56,9 +107,11 @@ public class Controller extends JPanel {
 
         this.turn = 1;
         view.statusLabel.setText(view.playerOneName.getText() + "'s turn");
+        chooseColor();
     }
 
     public void pressButton(int i, int j) {
+
         if (!model.validMove(i, j)) {
             if (turn % 2 == 1) {
                 view.statusLabel.setText("Invalid Move. " + view.playerOneName.getText() + "'s turn");
@@ -73,17 +126,42 @@ public class Controller extends JPanel {
 
             if (turn % 2 == 1) {
                 view.buttons[i][j].setText("X");
-                turn++;
                 view.statusLabel.setText(view.playerTwoName.getText() + "'s turn");
                 view.buttons[i][j].setForeground(newColor1);
+                view.buttons[i][j].setBackground(newColor1);
             }
             else if (turn % 2 == 0) {
                 view.buttons[i][j].setText("O");
-                turn++;
                 view.statusLabel.setText(view.playerOneName.getText() + "'s turn");
                 view.buttons[i][j].setForeground(newColor2);
+                view.buttons[i][j].setBackground(newColor2);
             }
+
+
+            if (model.checkWin(i, j, turn)) {
+                System.out.println("WINNER");
+                for (int x = 0; x<15; x++) {
+                    for (int y = 0; y<15; y++) {
+                        view.buttons[x][y].setEnabled(false);
+                    }
+                }
+
+                if (turn % 2 == 1) {
+                    view.statusLabel.setText(view.playerOneName.getText() + " Wins!");
+                }
+                else if (turn % 2 == 0) {
+                    view.buttons[i][j].setText("O");
+                    view.statusLabel.setText(view.playerTwoName.getText() + " Wins!");
+                    view.buttons[i][j].setForeground(newColor2);
+                }
+            }
+
+            turn++;
         }
+    }
+
+    public void makeMove() {
+
     }
 
     public static void main(String[] args) {
--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,8 @@
 public class Model {
     private char[][] board;
+    public static int WIN_COUNT = 5;
+    public static int COLUMNS = 15;
+    public static int ROWS = 15;
 
     public Model() {
         board = new char[15][15];
@@ -31,5 +34,107 @@ public class Model {
             return false;
         }
         return true;
+    }
+
+    public boolean checkWin(int i, int j, int turn) {
+
+        char playerSymbol;
+
+        if (turn % 2 == 1) {
+            playerSymbol = 'X';
+        }
+        else {
+            playerSymbol = 'O';
+        }
+
+        int consecutiveCounter = 0;
+
+        //HORIZONTAL
+        int leftBound = 0;
+        int rightBound = 0;
+        if (j - WIN_COUNT >= 0)
+            leftBound = j - WIN_COUNT;
+        else
+            leftBound = 0;
+
+        if (j + WIN_COUNT <= COLUMNS - 1)
+            rightBound = j + WIN_COUNT;
+        else
+            rightBound = COLUMNS - 1;
+
+        int jTraverse = j;
+
+        while (jTraverse >= leftBound && consecutiveCounter < WIN_COUNT) {
+            if (board[i][jTraverse] == playerSymbol) {
+                consecutiveCounter++;
+                jTraverse--;
+            }
+            else {
+                jTraverse = j+1;
+                break;
+            }
+        }
+
+        while (jTraverse <= rightBound && consecutiveCounter < WIN_COUNT) {
+            if (board[i][jTraverse] == playerSymbol) {
+                consecutiveCounter++;
+                jTraverse++;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (consecutiveCounter >= WIN_COUNT) {
+            return true;
+        }
+
+        //VERTICAL
+
+        consecutiveCounter = 0;
+        int upperBound = 0;
+        int lowerBound = 0;
+
+        if (i - WIN_COUNT >= 0)
+            upperBound = i - WIN_COUNT;
+        else
+            upperBound = 0;
+
+        if (i + WIN_COUNT <= ROWS - 1)
+            lowerBound = i + WIN_COUNT;
+        else
+            lowerBound = ROWS - 1;
+
+        int iTraverse = i;
+
+        while (iTraverse >= upperBound && consecutiveCounter < WIN_COUNT) {
+            if (board[iTraverse][j] == playerSymbol) {
+                consecutiveCounter++;
+                iTraverse--;
+            }
+            else {
+                iTraverse = i + 1;
+                break;
+            }
+        }
+
+        while (iTraverse <= lowerBound && consecutiveCounter < WIN_COUNT) {
+            if (board[iTraverse][j] == playerSymbol) {
+                consecutiveCounter++;
+                iTraverse++;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (consecutiveCounter >= WIN_COUNT) {
+            return true;
+        }
+
+        //DIAGONAL
+
+
+        return false;
     }
 }
